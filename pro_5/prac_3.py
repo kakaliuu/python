e39936056d1d0ababef25e9304aadcb921108fbd
kakaliuu <kakaliuu@163.com>
2019-02-24 19:37:08 +0800

    the first commit

--- pro_5/prac_3.py
+++ pro_5/prac_3.py
@@ -1,14 +1,20 @@
-import os
+# coding:utf-8
+
 import shutil
-path='./'
+import os
 
+path = './'
+files = os.listdir(path)
 
-for  f in files:
-    if   os.path.exists():
-        makefolder()
-        movefiletofolder()
-
+for f in files:
+    # f.png
+    #./png
+    # os.path.join()函数用来连接路径，与视频中的“+”号效果类似，但是前者可以避免不同操作系统对程序带来的影响
+    folder_name = os.path.join('.',f.split('.')[-1])
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+        shutil.move(f,folder_name)
     else:
-        movetofolder();
+        shutil.move(f,folder_name)
 
 
